f444ad5110b846e72a3c5ffd63983f4d4f1b2829
Arunvarma29 <varmaarun580@gmail.com>
2025-09-16 12:33:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB().then(() => {
+  app.listen(process.env.PORT || 7000, () => {
+    `⚙️ Server is running at port:${process.env.PORT}`;
+  });
+});
